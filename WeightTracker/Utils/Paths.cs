fff1dc2a08b92abc4d38b12ca14c0d8635875352
Jon Nyman <nymanjon@gmail.com>
2021-09-19 16:44:07 -0700

    Initial

---
+++ WeightTracker/Utils/Paths.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace WeightTracker.Utils
+{
+    public static class Paths
+    {
+        public static string GetAppDir()
+        {
+            var mealPlannerDirectoryName = "weight-tracker";
+            var localAppPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var path =
+                string.IsNullOrWhiteSpace(localAppPath)
+                    ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), ".local", mealPlannerDirectoryName)
+                : Path.Combine(localAppPath, mealPlannerDirectoryName);
+            Directory.CreateDirectory(path);
+            return path;
+        }
+    }
+}
