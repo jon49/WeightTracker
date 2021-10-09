5c45f80d9dc8eb23f6a37f6f0565cd483e8d40e6
Jon Nyman <nymanjon@gmail.com>
2021-10-08 17:26:34 -0700

    Added logged in end point.

---
+++ WeightTracker/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeightTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController  : BaseUserController
+    {
+        [HttpGet]
+        [Route("logged-in")]
+        public bool IsLoggedIn()
+            => UserId > 0;
+    }
+}
