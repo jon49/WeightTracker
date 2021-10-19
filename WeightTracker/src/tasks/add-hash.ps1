af8b22dc855c2b6f91b73f120c3d25cb022ad55e
Jon Nyman <nymanjon@gmail.com>
2021-10-18 21:54:26 -0700

    Fixed adding hash to add it by file hierarchy

--- WeightTracker/src/tasks/add-hash.ps1
+++ WeightTracker/src/tasks/add-hash.ps1
@@ -1,6 +1,36 @@
 param([string] $basePath = '../wwwroot/web')
+
+$filePaths = @(
+    "/lib/chart.min.js"
+    "/lib/db-safari.js"
+    "/lib/db.min.js"
+    "actions.js"
+    "h.js"
+    "snack-bar.js"
+    "utils.js"
+    "db.js"
+    "main.js"
+    "user-settings-page.js"
+    "charts-page.js"
+    "entry-page.js"
+)
+
 $files = @{}
-Get-ChildItem -Path $basePath -Recurse -Include *.js,*.css -Exclude sw.js |
+$filePaths | % {
+    $fullPath = Join-Path -Path $basePath "js" $_ | Get-Item
+    $content = Get-Content $fullPath
+    $files.Keys | % {
+        $content = $content -replace $_, $files.$_
+    }
+    $content | Set-Content -Path $fullPath
+    $hash = Get-FileHash $fullPath | % { $_.Hash.Substring($_.Hash.Length - 8).ToLower() }
+    $newFileName = "$($fullPath.BaseName).$hash$($fullPath.Extension)"
+    Rename-Item -Path $fullPath -NewName $newFileName
+    $files.Add($fullPath.Name, $newFileName)
+}
+
+
+Get-ChildItem -Path $basePath -Recurse -Include *.css |
 % {
     $hash = Get-FileHash $_ | % { $_.Hash.Substring($_.Hash.Length - 8).ToLower() }
     $newFileName = "$($_.BaseName).$hash$($_.Extension)"
@@ -8,7 +38,7 @@ Get-ChildItem -Path $basePath -Recurse -Include *.js,*.css -Exclude sw.js |
     Rename-Item -Path $_ -NewName $newFileName
 }
 
-Get-ChildItem -Path $basePath -Recurse -Include *.js,*.html |
+Get-ChildItem -Path $basePath -Recurse -Include *.html,sw.js |
 % {
     $content = Get-Content $_
     $files.Keys | % {
