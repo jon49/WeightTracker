923381428d5f924d18c5a46bdfd8d8d0437cc430
Jon Nyman <nymanjon@gmail.com>
2021-10-20 16:41:01 -0700

    Fixed count calculation for `dateFill`

--- WeightTracker/src/static/web/js/utils.js
+++ WeightTracker/src/static/web/js/utils.js
@@ -40,7 +40,7 @@ export function dateToString(date) {
 export function dateFill(from, to) {
     from = new Date(from)
     // 1e3*60*60*24 == 86,400,000 ms to days
-    const count = ((+to - +from)/864e5 + 1) | 0
+    const count = Math.ceil((+to - +from)/864e5 + 1)
     const dates = new Array(count)
     for (let index = 0; index < count; index++) {
         dates[index] = dateToString(from)
