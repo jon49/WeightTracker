b0e60e2089890af94142bc2a3db29d305c2b828c
Jon Nyman <nymanjon@gmail.com>
2022-05-19 16:03:27 -0700

    Update start to be easier to work with.

--- WeightTracker/src/web2/tasks/start.ps1
+++ WeightTracker/src/web2/tasks/start.ps1
@@ -1,9 +1,15 @@
 # Start-Job -ScriptBlock { esbuild .\src\web2\sw.ts --format=iife --bundle --outfile=./public/web2/sw.js --watch }
 
-param([switch]$Debug = $false)
+param([switch]$Debug = $false, [switch]$FullStack = $false)
 
 if ($Debug) {
     esbuild .\src\web2\sw.ts --format=iife --bundle --outfile=.\public\web2\sw.js --watch
-} else {
-    esbuild .\src\web2\sw.ts --format=iife --bundle --outfile=..\..\wwwroot\web2\sw.js --watch
+    return
 }
+
+if ($FullStack) {
+    esbuild .\src\web2\sw.ts --format=iife --bundle --outfile=..\..\wwwroot\web2\sw.js --watch
+    return
+}
+
+Write-Host "Please select 'Debug' or 'FullStack'"
