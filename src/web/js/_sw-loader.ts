fb082e36df2451775c9e47d04cd43e68e0accd87
Jon Nyman <nymanjon@gmail.com>
2023-12-11 13:28:59 -0700

    Update sw loader message to have a link in the toaster to update.

--- src/web/js/_sw-loader.ts
+++ src/web/js/_sw-loader.ts
@@ -2,20 +2,27 @@ import { notifier } from "@jon49/sw/new-app-notifier.js"
 
 notifier(notifyUserAboutNewVersion)
 
-function notifyUserAboutNewVersion(state = "", worker: ServiceWorker) {
+let worker : ServiceWorker | undefined
+function notifyUserAboutNewVersion(state = "", worker_: ServiceWorker) {
+    worker = worker_
     let nav = document.getElementById("sw-message")
     nav?.insertAdjacentHTML("afterbegin", `<div class=inline><a id=new-worker href="#">Click here to update your app.</a></div>`)
     let newWorkerLink = document.getElementById("new-worker")
-    function handleUpdateClick(e: Event) {
-        e.preventDefault()
-        worker.postMessage({ action: 'skipWaiting' })
-        newWorkerLink?.remove()
-    }
     newWorkerLink?.addEventListener("click", handleUpdateClick)
     if (state === "waiting") return
     // Publish custom event for "user-messages" to display a toast.
     document.dispatchEvent(new CustomEvent("user-messages", {
-        detail: ["A new version of the app is available. Click the link in the top right to update."]
+        detail: {
+            html: `A new version of the app is available. Click the link at the top of the page to update. <a data-action=update-service-worker style="color:blue;" href='#'>Or click here.</a>`,
+        }
     }))
+    let item = document.querySelector("[data-action='update-service-worker']")
+    if (!item) return
+    item.addEventListener("click", handleUpdateClick)
+}
+
+function handleUpdateClick(e: Event) {
+    e.preventDefault()
+    worker?.postMessage({ action: 'skipWaiting' })
 }
 
--- src/web/js/app.bundle.ts
+++ src/web/js/app.bundle.ts
@@ -12,19 +12,37 @@ doc.addEventListener("user-messages", (e: CustomEvent) => {
     let template = doc.getElementById("toast-template")
     let toasts = doc.getElementById("toasts")
     if (!(template && template instanceof HTMLTemplateElement) || !toasts) return
-    for (let message of e.detail) {
-        let clone = template.content.cloneNode(true)
-        let wordCount = message.split(" ").length
-        if (!(clone instanceof DocumentFragment)) return
-        let first = clone.firstElementChild
-        if (!first) return
-        first.setAttribute("data-timeout", ""+(1e3 + wordCount * 400))
-        let messageEl = first.querySelector(".message")
-        if (!messageEl) return
-        messageEl.textContent = message
-        toasts.appendChild(first)
+    if (Array.isArray(e.detail)) {
+        for (let message of e.detail) {
+            appendMessage(template, toasts, message)
+        }
+    }
+    if (e.detail.html) {
+        appendMessage(template, toasts, e.detail.html, true)
     }
 })
+
+function appendMessage(
+    template: HTMLTemplateElement,
+    toasts: HTMLElement,
+    message: string,
+    isHtml = false
+) {
+    let clone = template.content.cloneNode(true)
+    let wordCount = message.split(" ").length
+    if (!(clone instanceof DocumentFragment)) return
+    let first = clone.firstElementChild
+    if (!first) return
+    first.setAttribute("data-timeout", ""+(1e3 + wordCount * 400))
+    let messageEl = first.querySelector(".message")
+    if (!messageEl) return
+    if (isHtml) {
+        messageEl.innerHTML = message
+    } else {
+        messageEl.textContent = message
+    }
+    toasts.appendChild(first)
+}
 
 // @ts-ignore
 doc.addEventListener("app-theme", (e: CustomEvent) => {
@@ -33,5 +51,4 @@ doc.addEventListener("app-theme", (e: CustomEvent) => {
     if (theme === "neither") return
     doc.body.classList.add(theme)
 })
-
 
