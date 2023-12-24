6709acf6f1016cf8bcc59880ca010b5e7c7943fd
Jon Nyman <nymanjon@gmail.com>
2023-12-23 21:57:27 -0700

    Fix login

--- src/web/pages/index.html.ts
+++ src/web/pages/index.html.ts
@@ -1,10 +1,14 @@
 import html from "html-template-tag-stream"
 import layout from "./_layout.html.js"
 import { Route } from "@jon49/sw/routes.js"
+import db from "../server/global-model.js"
 
 const route: Route = {
     route: /\/web\/?$/,
-    get: async () => {
+    get: async ({ query }) => {
+        if (query.login === "success") {
+            db.setLoggedIn(true)
+        }
         return layout({
             main: html`<p>Welcome to weight tracking!</p>`,
             title: "Weight Tracking",
