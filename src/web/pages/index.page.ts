a3d367bf11c3ccbdac9e210d82d63d9019e8704d
Jon Nyman <nymanjon@gmail.com>
2024-11-06 22:24:00 -0700

    Set login in the routing of the URL. Update packages with new routing configs.

--- src/web/pages/index.page.ts
+++ src/web/pages/index.page.ts
@@ -3,14 +3,10 @@ import { RoutePage } from "@jon49/sw/routes.js"
 let {
     html,
     layout,
-    globalDb: db,
 } = self.app
 
 const route: RoutePage = {
-    get: async ({ query }) => {
-        if (query.login === "success") {
-            db.setLoggedIn(true)
-        }
+    get: async () => {
         return layout({
             main: html`<p>Welcome to weight tracking!</p>`,
             title: "Weight Tracking",
